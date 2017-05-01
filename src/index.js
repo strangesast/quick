9ae10d33dafb1d212072f9f2ada32302100aed45
Sam Zagrobelny <strangesast@gmail.com>
2017-05-01 11:17:53 -0400

    moved to svg

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 require('./index.less');
-require('../assets/rocky.png');
-require('../assets/energy.png');
-require('../assets/flame.png');
-require('../assets/general.png');
-require('../assets/snowflake.png');
-require('../assets/water.png');
 
 import * as d3 from 'd3';
 
