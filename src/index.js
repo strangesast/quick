6f97815bbb2bc2f709dbb6e479009ceb4514804b
Sam Zagrobelny <strangesast@gmail.com>
2017-05-01 08:42:29 -0400

    added background images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-require('../assets/rocky.png');
 require('./index.less');
+require('../assets/rocky.png');
+require('../assets/energy.png');
+require('../assets/flame.png');
+require('../assets/general.png');
+require('../assets/snowflake.png');
+require('../assets/water.png');
 
 import * as d3 from 'd3';
 
